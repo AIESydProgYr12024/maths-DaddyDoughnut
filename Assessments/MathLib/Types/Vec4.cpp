358f9089adc44074ab2746037780fc650bc472a3
Josh Hodgetts <daddydoughnut3000@gmail.com>
2024-04-26 22:00:46 +1000

    #5 vector4

--- Assessments/MathLib/Types/Vec4.cpp
+++ Assessments/MathLib/Types/Vec4.cpp
@@ -27,49 +27,64 @@ namespace MathLib
 	}
 
 	Vec4::Vec4(Vector4 _value)
+		: x{ _value.x }, y{ _value.y }, z{ _value.z }, w{ _value.w }
 	{
 	}
 
 	Vec4::Vec4(const Vec4& _other)
+		: x{ _other.x }, y{ _other.y }, z{ _other.z }, w{ _other.w }
 	{
 	}
 
 	Vec4::Vec4(Vec4&& _other) noexcept
+		: x{ _other.x }, y{ _other.y }, z{ _other.z }, w{ _other.w }
 	{
+		_other.x = 0.f;
+		_other.y = 0.f; 
+		_other.z = 0.f;
+		_other.w = 0.f;
+
 	}
 
-	Vec4::~Vec4()
-	{
-	}
+	Vec4::~Vec4() = default;
 
 	Vec4 Vec4::Add(const Vec4& _lhs, const Vec4& _rhs)
 	{
-		return {};
+		return { _lhs.x + _rhs.x, _lhs.y + _rhs.y, _lhs.z + _rhs.z, _lhs.w + _rhs.w };
 	}
 
 	Vec4 Vec4::Subtract(const Vec4& _lhs, const Vec4& _rhs)
 	{
-		return {};
+		return { _lhs.x - _rhs.x, _lhs.y - _rhs.y, _lhs.z - _rhs.z, _lhs.w - _rhs.w };
 	}
 
 	Vec4 Vec4::Scale(const Vec4& _lhs, float _rhs)
 	{
-		return {};
+		return { _lhs.x * _rhs, _lhs.y * _rhs, _lhs.z * _rhs, _lhs.w * _rhs };
 	}
 
 	Vec4 Vec4::Normalised(const Vec4& _vec)
 	{
-		return {};
+		const float mag = _vec.Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			return Vec4{ 0.f, 0.f, 0.f, 0.f };
+		}
+		else
+		{
+			return Vec4{ _vec.x / mag, _vec.y / mag, _vec.z / mag, _vec.w / mag };
+		}
 	}
 
 	float Vec4::Distance(const Vec4& _a, const Vec4& _b)
 	{
-		return 0;
+		return (_a - _b).Magnitude();
 	}
 
 	float Vec4::Dot(const Vec4& _lhs, const Vec4& _rhs)
 	{
-		return 0;
+		return (_lhs.x * _rhs.x) + (_lhs.y * _rhs.y) + (_lhs.z * _rhs.z) + (_lhs.w * _rhs.w);
 	}
 
 	Vec4 Vec4::Lerp(const Vec4& _a, const Vec4& _b, float _t)
@@ -79,90 +94,152 @@ namespace MathLib
 
 	float Vec4::Dot(const Vec4& _rhs) const
 	{
-		return 0;
+		return (x * _rhs.x) + (y * _rhs.y) + (z * _rhs.z) + (w * _rhs.w);
 	}
 
 	float Vec4::Magnitude() const
 	{
-		return 0;
+		return sqrt((x * x) + (y * y) + (z * z) + (w * w));
 	}
 
 	float Vec4::MagnitudeSqr() const
 	{
-		return 0;
+		return (x * x) + (y * y) + (z * z) + (w * w);
 	}
 
 	void Vec4::Normalise()
 	{
+		const float mag = Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			x = 0.f;
+			y = 0.f;
+			z = 0.f;
+			w = 0.f;
+		}
+		else
+		{
+			x = x / mag;
+			y = y / mag;
+			z = z / mag;
+			w = w / mag;
+		}
+
 	}
 
 	Vec4 Vec4::Normalised() const
 	{
-		return {};
+		const float mag = Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			return Vec4{ 0.f, 0.f, 0.f, 0.f };
+		}
+		else
+		{
+			return Vec4{ x / mag, y / mag, z / mag, w / mag };
+		}
 	}
 
 	string Vec4::ToString() const
 	{
-		return "";
+		return string("(") + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ", " + std::to_string(w) + string("(");
 	}
 
 	Vec4::operator Vector4() const
 	{
-		return {};
+		return { x, y, z, w};
 	}
 
 	bool Vec4::operator==(const Vec4& _other) const
 	{
-		return false;
+		return Compare(x, _other.x) && Compare(y, _other.y) && Compare(z, _other.z) && Compare(w, _other.w);
 	}
 
 	bool Vec4::operator!=(const Vec4& _other) const
 	{
-		return false;
+		return !(*this == _other);
 	}
 
 	Vec4 Vec4::operator-(const Vec4& _other) const
 	{
-		return {};
+		return { x - _other.x, y - _other.y, z - _other.z, w - _other.w };
 	}
 
 	Vec4& Vec4::operator-=(const Vec4& _other)
 	{
+		x -= _other.x;
+		y -= _other.y;
+		z -= _other.z;
+		w -= _other.w;
+
 		return *this;
 	}
 
 	Vec4 Vec4::operator+(const Vec4& _other) const
 	{
-		return {};
+		return { x + _other.x, y + _other.y, z + _other.z, w + _other.w };
 	}
 
 	Vec4& Vec4::operator+=(const Vec4& _other)
 	{
+		x += _other.x; 
+		y += _other.y; 
+		z += _other.z;
+		w += _other.w;
+
 		return *this;
 	}
 
 	Vec4 Vec4::operator*(float _other) const
 	{
-		return {};
+		return { x * _other, y * _other, z * _other, w * _other };
 	}
 
 	Vec4& Vec4::operator*=(float _other)
 	{
+		x *= _other; 
+		y *= _other; 
+		z *= _other;
+		w *= _other;
+
 		return *this;
 	}
 
 	Vec4& Vec4::operator=(const Vec4& _other)
 	{
+		if (*this == _other)
+			return *this;
+
+		x = _other.x;
+		y = _other.y;
+		z = _other.z;
+		w = _other.w;
+
 		return *this;
 	}
 
 	Vec4& Vec4::operator=(Vec4&& _other) noexcept
 	{
+		if (*this == _other)
+			return *this;
+
+		x = _other.x;
+		y = _other.y;
+		z = _other.z;
+		w = _other.w;
+
+		_other.x = 0.f;
+		_other.y = 0.f;
+		_other.z = 0.f;
+		_other.w = 0.f;
+
 		return *this;
 	}
 
 	Vec4 operator*(float _lhs, const Vec4& _rhs)
 	{
-		return {};
+		return {_lhs * _rhs.x, _lhs * _rhs.y, _lhs * _rhs.z, _lhs * _rhs.w };
 	}
 }
