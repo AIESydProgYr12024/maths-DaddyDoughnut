2e31e360d159a919c58d2c2b6093fc42cba523a5
DD04 <101507209+DaddyDoughnut@users.noreply.github.com>
2024-04-26 16:48:54 +1000

    #4 vec3

--- Assessments/MathLib/Types/Vec3.cpp
+++ Assessments/MathLib/Types/Vec3.cpp
@@ -24,7 +24,7 @@ namespace MathLib
 	{
 		for (size_t i = 0; i < VEC_3_SIZE; ++i)
 		{
-			data[i] = *(_values.begin() + i);
+			data[i] = * (_values.begin() + i);
 		}
 	}
 
@@ -52,37 +52,50 @@ namespace MathLib
 
 	Vec3 Vec3::Add(const Vec3& _lhs, const Vec3& _rhs)
 	{
-		return {};
+		return { _lhs.x + _rhs.x, _lhs.y + _rhs.y, _lhs.z + _rhs.z };
 	}
 
 	Vec3 Vec3::Subtract(const Vec3& _lhs, const Vec3& _rhs)
 	{
-		return {};
+		return { _lhs.x - _rhs.x, _lhs.y - _rhs.y, _lhs.z - _rhs.z };
 	}
 
 	Vec3 Vec3::Scale(const Vec3& _lhs, float _rhs)
 	{
-		return {};
+		return { _lhs.x * _rhs, _lhs.y * _rhs, _lhs.z * _rhs };
 	}
 
 	Vec3 Vec3::Normalised(const Vec3& _vec)
 	{
-		return {};
+		const float mag = _vec.Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			return Vec3{ 0.f, 0.f, 0.f };
+		}
+		else
+		{
+			return Vec3{ _vec.x / mag, _vec.y / mag, _vec.z / mag };
+		}
 	}
 
 	float Vec3::Distance(const Vec3& _a, const Vec3& _b)
 	{
-		return 0;
+		return (_a - _b).Magnitude();
 	}
 
 	float Vec3::Dot(const Vec3& _lhs, const Vec3& _rhs)
 	{
-		return 0;
+		return (_lhs.x * _rhs.x) + (_lhs.y * _rhs.y) + (_lhs.z * _rhs.z);
 	}
 
 	Vec3 Vec3::Cross(const Vec3& _lhs, const Vec3& _rhs)
 	{
-		return {};
+		return {
+			(_lhs.y * _rhs.z) - (_lhs.z * _rhs.y),
+			(_lhs.x * _rhs.z) - (_lhs.z * _rhs.x),
+			(_lhs.y * _rhs.x) - (_lhs.x * _rhs.y)
+		};
 	}
 
 	Vec3 Vec3::Lerp(const Vec3& _a, const Vec3& _b, float _t)
@@ -92,36 +105,63 @@ namespace MathLib
 
 	float Vec3::Dot(const Vec3& _rhs) const
 	{
-		return 0;
+		return (x * _rhs.x) + (y * _rhs.y) + (z * _rhs.z);
 	}
 
 	Vec3 Vec3::Cross(const Vec3& _rhs) const
 	{
-		return {};
+		return {
+			(y * _rhs.z) - (z * _rhs.y),
+			(x * _rhs.z) - (z * _rhs.x),
+			(y * _rhs.x) - (x * _rhs.y)
+		};
 	}
 
 	float Vec3::Magnitude() const
 	{
-		return 0;
+		return sqrt((x * x) + (y * y) + (z * z));
 	}
 
 	float Vec3::MagnitudeSqr() const
 	{
-		return 0;
+		return (x * x) + (y * y) + (z * z);
 	}
 
 	void Vec3::Normalise()
 	{
+		const float mag = Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			x = 0.f;
+			y = 0.f;
+			z = 0.f;
+		}
+		else
+		{
+			x = x / mag;
+			y = y / mag;
+			z = z / mag;
+		}
 	}
 
 	Vec3 Vec3::Normalised() const
 	{
-		return {};
+		const float mag = Magnitude();
+
+		if (Compare(mag, 0.f))
+		{
+			return Vec3{ 0.f, 0.f, 0.f };
+		}
+		else
+		{
+			return Vec3{ x / mag, y / mag, z / mag };
+		}
 	}
 
 	string Vec3::ToString() const
 	{
-		return "";
+		return string("(") + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + string("(");
 	}
 
 	Vec3::operator Vector3() const
@@ -141,31 +181,43 @@ namespace MathLib
 
 	Vec3 Vec3::operator-(const Vec3& _other) const
 	{
-		return {};
+		return { x - _other.x, y - _other.y, z - _other.z };
 	}
 
 	Vec3& Vec3::operator-=(const Vec3& _other)
 	{
+		x -= _other.x;
+		y -= _other.y;
+		z -= _other.z;
+
 		return *this;
 	}
 
 	Vec3 Vec3::operator+(const Vec3& _other) const
 	{
-		return {};
+		return { x + _other.x, y + _other.y, z + _other.z };
 	}
 
 	Vec3& Vec3::operator+=(const Vec3& _other)
 	{
+		x += _other.x;
+		y += _other.y;
+		z += _other.z;
+
 		return *this;
 	}
 
 	Vec3 Vec3::operator*(float _other) const
 	{
-		return {};
+		return { x * _other, y * _other, z * _other };
 	}
 
 	Vec3& Vec3::operator*=(float _other)
 	{
+		x *= _other;
+		y *= _other;
+		z *= _other;
+
 		return *this;
 	}
 
